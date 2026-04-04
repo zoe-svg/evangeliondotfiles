f5f51b3b649277f27dc6807911a5dfb132728d53
Zoe Omofia <zoe.omorfia@gmail.com>
2026-04-03 20:56:29 -0500

    Add files via upload

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,45 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion d7af5f
+set --global fish_color_cancel --reverse
+set --global fish_color_command fc9d2e
+set --global fish_color_comment 8de7a7
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end 8de7a7
+set --global fish_color_error ff0000
+set --global fish_color_escape brcyan
+set --global fish_color_hg_added green
+set --global fish_color_hg_clean green
+set --global fish_color_hg_copied magenta
+set --global fish_color_hg_deleted red
+set --global fish_color_hg_dirty red
+set --global fish_color_hg_modified yellow
+set --global fish_color_hg_renamed magenta
+set --global fish_color_hg_unmerged red
+set --global fish_color_hg_untracked yellow
+set --global fish_color_history_current --bold
+set --global fish_color_host --reset
+set --global fish_color_host_remote yellow
+set --global fish_color_normal --reset
+set --global fish_color_operator brcyan
+set --global fish_color_param 710C04
+set --global fish_color_quote 710C04
+set --global fish_color_redirection fc9d2e --bold
+set --global fish_color_search_match white --bold --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_description yellow --italics
+set --global fish_pager_color_prefix --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_selected_background --reverse
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
